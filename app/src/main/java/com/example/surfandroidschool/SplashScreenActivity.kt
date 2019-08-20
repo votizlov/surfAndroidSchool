6dfef3f8d5a8e61b13f5e4c2b94f3471a5aa7c38
Votizlov <SeriousFox@yandex.ru>
2019-08-20 21:24:52 +0300

    added memes activity

--- app/src/main/java/com/example/surfandroidschool/SplashScreenActivity.kt
+++ app/src/main/java/com/example/surfandroidschool/SplashScreenActivity.kt
@@ -1,13 +1,21 @@
  package com.example.surfandroidschool
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.Window
+import java.util.*
+import kotlin.concurrent.schedule
 
  class SplashScreenActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val intent = Intent(this, LoginActivity::class.java)
+        Timer("Show time", false).schedule(300){
+            startActivity(intent)
+        }
     }
+
+
 }
