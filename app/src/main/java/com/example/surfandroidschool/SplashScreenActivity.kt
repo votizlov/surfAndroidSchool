78e0763b49169642032ed8b46bf665dc629666f8
Votizlov <SeriousFox@yandex.ru>
2019-08-20 20:51:56 +0300

    added login activity & loading splash screen

--- app/src/main/java/com/example/surfandroidschool/SplashScreenActivity.kt
+++ app/src/main/java/com/example/surfandroidschool/SplashScreenActivity.kt
@@ -1,9 +1,10 @@
-package com.example.surfandroidschool
+ package com.example.surfandroidschool
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Window
 
-class MainActivity : AppCompatActivity() {
+ class SplashScreenActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
