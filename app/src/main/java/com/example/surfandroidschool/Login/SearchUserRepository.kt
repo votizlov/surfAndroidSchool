a38c86ac10a5e575bc0acfac488fda445e257799
Votizlov <seriousfox@yandex.ru>
2019-08-24 09:55:53 +0300

    added user repo

--- app/src/main/java/com/example/surfandroidschool/Login/SearchUserRepository.kt
+++ app/src/main/java/com/example/surfandroidschool/Login/SearchUserRepository.kt
@@ -1,11 +1,9 @@
 package com.example.surfandroidschool.Login
 
+
 import com.example.surfandroidschool.NetworkService
 
-class SearchUserRepository(val userAPI: UserAPI) {
-        fun searchUser(location: String, language: String) {
+class SearchUserRepository(phone:String,pass:String) {
             val userAPI = NetworkService.createUserAPI()
-
+    val code = userAPI.search(phone,pass)
         }
-
-}
--- app/src/main/java/com/example/surfandroidschool/Login/UserAPI.kt
+++ app/src/main/java/com/example/surfandroidschool/Login/UserAPI.kt
@@ -1,12 +1,10 @@
 package com.example.surfandroidschool.Login
 
-import retrofit2.http.Body
 import retrofit2.http.GET
 import retrofit2.http.Query
 
 public interface UserAPI {
     @GET("/auth/login")
-    fun search(@Query("phone") phone:Int,
-               @Query("pass") pass:String):Boolean<Result>
-    )
+    fun search(@Query("phone") phone:String,
+               @Query("pass") pass:String):Int
 }
