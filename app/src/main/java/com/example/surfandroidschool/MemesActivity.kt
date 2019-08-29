7bb573a2c0a800b9a8166c182ab644f6dc559be9
Votizlov <SeriousFox@yandex.ru>
2019-08-29 19:06:56 +0300

    painted status bar

--- app/src/main/java/com/example/surfandroidschool/MemesActivity.kt
+++ app/src/main/java/com/example/surfandroidschool/MemesActivity.kt
@@ -1,6 +1,9 @@
 package com.example.surfandroidschool
 
+import android.os.Build
 import android.os.Bundle
+import android.view.autofill.AutofillValue
+import androidx.annotation.RequiresApi
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_memes.*
@@ -10,25 +13,23 @@ class MemesActivity : AppCompatActivity() {
     private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
         when (item.itemId) {
             R.id.navigation_home -> {
-                message.setText(R.string.title_home)
                 return@OnNavigationItemSelectedListener true
             }
             R.id.navigation_dashboard -> {
-                message.setText(R.string.title_dashboard)
                 return@OnNavigationItemSelectedListener true
             }
             R.id.navigation_notifications -> {
-                message.setText(R.string.title_notifications)
                 return@OnNavigationItemSelectedListener true
             }
         }
         false
     }
 
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_memes)
-
+        recyclerView.autofill(AutofillValue.forList(3))
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
     }
 
